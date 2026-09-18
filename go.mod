54865ad4d2acdbc7f127ede96eb11f66a154943f
annyark29-cloud <anastasiaarkeeva29@gmail.com>
2026-09-18 14:59:51 +0300

    Homework 1: two sum

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/annyark29-cloud/go-from-vk
+
+go 1.27.1
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	seen := make(map[int]int)
+	for i, num := range nums {
+		if j, ok := seen[target-num]; ok {
+			return []int{j, i}
+		}
+		seen[num] = i
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+}
